27946689606c0c3aaa1ec3a82bac4a6a9b8caa14
BhavanKumar05 <64483172+BhavanKumar05@users.noreply.github.com>
2020-06-17 12:57:13 +0530

    f

---
+++ index.php
@@ -0,0 +1,13 @@
+<body bgcolor="yellow">
+<pre>  
+<img src=""   height=200   width=300 />
+
+<?php
+
+print "Welcome to task1..!!\n";
+print "Stay home.!!\n";
+print "Stay safe.!!\n";
+
+
+?>
+</pre>
